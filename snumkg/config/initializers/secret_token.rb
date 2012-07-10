636485ddfb4f91a1be33ede284d7426a6a280498
Taegeon Um <taegeonum@gmail.com>
2012-07-10 11:17:57 +0900

    rails initial commit

---
+++ snumkg/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snumkg::Application.config.secret_token = 'cfca6804aef9bd843e72baadbfe575192295e2f84753e5d1ab02e3bfec07ca25876349d0af89f1c7eaec0e54bf4ace145bfe2c49c0c14c1afcf791dab965501b'
