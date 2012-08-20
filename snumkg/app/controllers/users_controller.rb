c8fc094067ba7ee922f733113ccd1062a257f076
Taegeon Um <taegeonum@gmail.com>
2012-08-20 13:01:12 +0900

    알림 보여주기 완료(클릭한 알람만 없어지도록)

--- snumkg/app/controllers/users_controller.rb
+++ snumkg/app/controllers/users_controller.rb
@@ -44,14 +44,14 @@ class UsersController < ApplicationController
         # 글에 댓글이 달렸을때
         # 내 프로필에 댓글이 달릴 때
         # 소꼬지 게시글에 참석한다고 할때
-        Alarm.where(:alarm_type => type, :acceptor_id => 2)
+        Alarm.where(:alarm_type => type, :acceptor_id => current_user.id)
               .order("CREATED_AT DESC")
               .group_by(&:article_id)
               .map {|article_id,alarms| alarms.group_by(&:new)}
 
       when 2
         # 내 댓글이 추천받을 때
-        Alarm.where(:alarm_type => type, :acceptor_id => 2)
+        Alarm.where(:alarm_type => type, :acceptor_id => current_user.id)
               .order("CREATED_AT DESC")
               .group_by(&:comment_id)
               .map {|a,alarms| alarms.group_by(&:new)}
@@ -71,20 +71,39 @@ class UsersController < ApplicationController
     @new_alarms = Array.new
     @old_alarms = Array.new
 
+    @all_alarms = Array.new
 
     # 여러명이 같은 글에 추천했을 때, 
     # 알람을 모아서 보여주자.
     types = [0, 1, 2, 3, 4]
     types.each do |type|
-      push_new_old_alarms(select_alarm(type), @new_alarms, @old_alarms)
+      @all_alarms = @all_alarms + select_alarm(type)
+      #push_new_old_alarms(select_alarm(type), @new_alarms, @old_alarms)
     end
+
     # new_alarms는 hash 배열. 
     # [{true => [...], false => [....]}, {true => [...], false => [...]}]
     #  -------article_id = 1------, ---------article_id = 2----
     #
     # new 값이 true를 가진 값들은 new_alarms에 삽입됨
     # hash key: true/false, value: 알람 배열
-    new_count = @new_alarms.count
+
+    new_count = 0
+    
+    @all_alarms.map! do |alarms|
+      unless alarms[true].nil?
+        new_count = new_count + 1
+        if alarms[false].nil?
+          {:new => true, :alarms => alarms[true].uniq { |alarm| alarm.alarmer}}
+        else
+          {:new => true, :alarms => (alarms[true] + alarms[false] ).uniq {|alarm| alarm.alarmer}}
+        end
+      else
+        {:new => false, :alarms => alarms[false].uniq {|alarm| alarm.alarmer}}
+      end
+    end
+
+=begin
     @new_alarms.map! do |alarms|
       for alarm in alarms[true]
         #alarm.update_attribute(:new, false) # new컬럼 값 false로 변경
@@ -102,8 +121,8 @@ class UsersController < ApplicationController
       # 보여줄땐 하나만 보여주도록
       {:new => false, :alarms => alarms[false].uniq {|alarm| alarm.alarmer}}
     end
-
-    @all_alarms = @new_alarms + @old_alarms
+=end
+    #@all_alarms = @new_alarms + @old_alarms
     #all_alarms 최근 순서대로 정렬
     @all_alarms.sort! do |a,b|
       a[:alarms][0].created_at < b[:alarms][0].created_at ? 1 : -1
